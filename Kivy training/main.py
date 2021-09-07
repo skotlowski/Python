f30235b3ea18fc8f85fa7535003f1fbfb8ac6283
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2021-09-07 21:59:12 +0200

    nested_loops_lab

--- Kivy training/main.py
+++ Kivy training/main.py
@@ -2,12 +2,17 @@ from kivy.app import App
 from kivy.uix.button import Button
 
 
+class SpecialButton(Button):
+    def __init__(self, **kwargs):
+        super(SpecialButton, self).__init__(**kwargs)
+        self.text = 'Button'
+        self.pos = (50, 50)
+        self.size_hint = (0.1, 0.1)
+
+
 class FirstKivyApp(App):
     def build(self):
-        return Button(
-            text='Button',
-            pos=(50, 50),
-            size_hint=(0.1, 0.10))
+        return SpecialButton()
 
 
 if __name__ == '__main__':
